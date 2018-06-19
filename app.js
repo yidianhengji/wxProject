e31a37ec556bab5dc1a7747bc5fba6aed66a7ec9
penggui <493871850@qq.com>
2018-06-19 11:37:18 +0800

    sss

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ App({
     // 登录
     wx.login({
       success: res => {
+        var code = res //返回code
+        console.log(code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
@@ -21,6 +23,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
+              console.log(res)
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
